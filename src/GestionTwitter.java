14478b4b36770e305dcd355750c8cd7ce2c71845
eeguskiza <e.eguskiza@opendeusto.es>
2023-11-04 19:38:28 +0100

    punto 7 de el tree

--- src/GestionTwitter.java
+++ src/GestionTwitter.java
@@ -1,6 +1,5 @@
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class GestionTwitter {
 	private static Map<String, UsuarioTwitter> usuariosPorId = new HashMap<>();
@@ -8,11 +7,14 @@ public class GestionTwitter {
 
 	public static void main(String[] args) {
 		try {
-			String fileName = "data2.csv";
+			String fileName = "/Users/erikeguskiza/Desktop/data.csv";
+			//String fileName = "data2.csv";
 			CSV.processCSV(new File(fileName));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		analizarAmigosUsuarios();
+		analizarAmigosUsuarios2();
 	}
 
 	public static boolean agregarUsuarioSiNoExiste(UsuarioTwitter usuario) {
@@ -44,4 +46,91 @@ public class GestionTwitter {
 	public static UsuarioTwitter buscarUsuarioPorId(String id) {
 		return usuariosPorId.get(id);
 	}
+
+	public static void analizarAmigosUsuarios() {
+		// Usaremos un TreeMap para ordenar automáticamente los usuarios por nick
+		TreeMap<String, UsuarioTwitter> ordenadoPorNick = new TreeMap<>(usuariosPorNick);
+
+		int totalUsuariosConAmigosDentro = 0;
+
+		for (UsuarioTwitter usuario : ordenadoPorNick.values()) {
+			List<String> amigosIds = usuario.getFriends();
+			int amigosDentro = 0;
+			int amigosFuera = 0;
+
+			for (String amigoId : amigosIds) {
+				if (usuariosPorId.containsKey(amigoId)) {
+					amigosDentro++;
+				} else {
+					amigosFuera++;
+				}
+			}
+
+			// Solo mostramos los usuarios que tienen amigos dentro del sistema
+			if (amigosDentro > 0) {
+				System.out.println("Usuario " + usuario.getScreenName() + " tiene " +
+						amigosFuera + " amigos fuera de nuestro sistema y " +
+						amigosDentro + " dentro.");
+				totalUsuariosConAmigosDentro++;
+			}
+		}
+
+		System.out.println("Hay " + totalUsuariosConAmigosDentro + " con algunos amigos dentro de nuestro sistema.");
+	}
+
+
+	public static void analizarAmigosUsuarios2() {
+		// Creamos un TreeSet con un Comparator personalizado
+		Comparator<UsuarioTwitter> comparador = new Comparator<UsuarioTwitter>() {
+			@Override
+			public int compare(UsuarioTwitter u1, UsuarioTwitter u2) {
+				int amigosDentroU1 = contarAmigosDentro(u1);
+				int amigosDentroU2 = contarAmigosDentro(u2);
+
+				// Primero comparamos por la cantidad de amigos dentro del sistema, en orden descendente
+				if (amigosDentroU1 != amigosDentroU2) {
+					return Integer.compare(amigosDentroU2, amigosDentroU1);
+				}
+
+				// Si tienen la misma cantidad de amigos dentro, comparamos por nick
+				return u1.getScreenName().compareTo(u2.getScreenName());
+			}
+
+			// Función auxiliar para contar los amigos dentro del sistema
+			private int contarAmigosDentro(UsuarioTwitter usuario) {
+				int amigosDentro = 0;
+				for (String amigoId : usuario.getFriends()) {
+					if (usuariosPorId.containsKey(amigoId)) {
+						amigosDentro++;
+					}
+				}
+				return amigosDentro;
+			}
+		};
+
+		Set<UsuarioTwitter> ordenadoPorAmigosYNick = new TreeSet<>(comparador);
+
+		for (UsuarioTwitter usuario : usuariosPorNick.values()) {
+			ordenadoPorAmigosYNick.add(usuario);
+		}
+
+		// Imprimir los usuarios del TreeSet
+		for (UsuarioTwitter usuario : ordenadoPorAmigosYNick) {
+			System.out.println(usuario.getScreenName() + " - " +
+					contarAmigosDentro(usuario) + " amigos.");
+		}
+	}
+
+	// Función auxiliar para contar los amigos dentro del sistema
+	private static int contarAmigosDentro(UsuarioTwitter usuario) {
+		int amigosDentro = 0;
+		for (String amigoId : usuario.getFriends()) {
+			if (usuariosPorId.containsKey(amigoId)) {
+				amigosDentro++;
+			}
+		}
+		return amigosDentro;
+	}
+
+
 }
